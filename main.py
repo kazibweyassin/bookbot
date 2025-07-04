f8d9b3ae28aea3947d691ffcfaf37c555535bdbd
yassinkazibwe <kazibweusama@gmail.com>
2025-07-04 09:38:54 +0300

    word count

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 def get_book_text(path):
     with open(path, 'r', encoding='utf-8') as f:
         return f.read()
+    
+
+def analyse_text(text):
+    word_count = len((text.split()))
+    print(f"{word_count} words found in the document")
+    return{
+        "word_count": word_count
+    }
+
 
 def main():
-    path = r"d:\Personal Projects\Backend\bookbot\books\frankenstein.txt" 
-    text = get_book_text(path)
-    print(text)
+    __path__ = r"d:\Personal Projects\Backend\bookbot\books\frankenstein.txt"  # Use raw string for Windows paths
+    text = get_book_text(__path__)
+
+    analysis = analyse_text(text)
+    print(f"Analysis: {analysis}")
 
 if __name__ == "__main__":
     main()
